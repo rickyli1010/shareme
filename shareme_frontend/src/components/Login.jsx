807be2b00864533b56c216c60d93cbb1cfa6fb9b
rickyli1010 <ricky_0321@hotmail.com>
2023-12-14 16:54:05 -0600

    login flow

---
+++ shareme_frontend/src/components/Login.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { GoogleLogin } from '@react-oauth/google';
+import shareVideo from '../assets/share.mp4';
+import logo from '../assets/logowhite.png';
+import { jwtDecode } from 'jwt-decode';
+import { client } from '../client';
+
+const Login = () => {
+  const navigate = useNavigate();
+  const responseGoogle = (response) => {
+    const decoded = jwtDecode(response.credential);
+    const { name, picture, sub } = decoded;
+
+    const user = {
+      _id: sub,
+      _type: 'user',
+      userName: name,
+      image: picture
+    };
+
+    localStorage.setItem('user', user);
+
+    client.createIfNotExists(user).then(() => {
+      navigate('/', { replace: true });
+    });
+  };
+
+  return (
+    <div className='flex justify-start items-center flex-col h-screen'>
+      <div className='relative w-full h-full'>
+        <video
+          src={shareVideo}
+          type='video/mp4'
+          loop
+          controls={false}
+          muted
+          autoPlay
+          className='w-full h-full object-cover'
+        />
+      </div>
+      <div className='absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0 bg-blackOverlay'>
+        <div className='p-5'>
+          <img src={logo} width='130px' alt='logo' />
+        </div>
+        <div className='shadow-2xl'>
+          <GoogleLogin onSuccess={responseGoogle} onError={responseGoogle} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
