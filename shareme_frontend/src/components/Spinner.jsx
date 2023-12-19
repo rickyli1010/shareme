c55a89e4e744be2e99d244bbe4b8aa4925828286
rickyli1010 <ricky_0321@hotmail.com>
2023-12-19 16:47:29 -0600

    pin page

---
+++ shareme_frontend/src/components/Spinner.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Circles } from 'react-loader-spinner';
+
+const Spinner = ({ message }) => {
+  return (
+    <div className='flex flex-col justify-center items-center w-full h-full'>
+      <div className='m-5'>
+        <Circles color='#00BFFF' height={50} width={200} />
+      </div>
+
+      <p className='text-lg text-center px-2'>{message}</p>
+    </div>
+  );
+};
+
+export default Spinner;
